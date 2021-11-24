8f3e0fca74aebc7df23e6d22b48b72a67270ba15
Ethievant Natacha <natacha.ethievant@gmail.com>
2021-11-24 12:16:36 +0100

    Avancement TestTicketMachine.java

--- Transport/src/test/java/fr/ufc/l3info/oprog/TestTicketMachine.java
+++ Transport/src/test/java/fr/ufc/l3info/oprog/TestTicketMachine.java
@@ -1,10 +1,17 @@
 package fr.ufc.l3info.oprog;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestTicketMachine {
     ITicket t;
+    ITicket base;
+    ITicket adjusted;
+    @Before
+    public void decl(){
+        base = buyTicket(false,100);
+    }
 
     @Test
     public void buyTicketBaseTicket(){
@@ -35,5 +42,27 @@ public class TestTicketMachine {
         t = buyTicket(true,-600);
         Assert.assertNull(t);
     }
+
+    @Test
+    public void adjustFareAjoutBaseTicket(){
+        adjusted = adjustFare(base,100);
+        boolean bol = false;
+        if (t instanceof AdjustedTicket) bol = true;
+        Assert.assertTrue(bol);
+        Assert.assertEquals(200, adjusted.getAmount());
+    }
+
+    @Test
+    public void adjustFareNull(){
+        adjusted = adjustFare(null,100);
+        Assert.assertNull(adjusted);
+    }
+
+    //TODO : Finir les tests adjustedFare somme ajout 0 ou inf et ticket non ajustable
+    @Test
+    public void adjustFareNoSuperieur0(){
+
+    }
+
 
 }
