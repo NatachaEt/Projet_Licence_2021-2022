a00bd328f290989f04a5f8f03daae1acaff450b4
Ethievant Natacha <natacha.ethievant@gmail.com>
2021-11-25 20:40:30 +0100

    Test et TicketMachine

---
+++ Transport/src/main/java/fr/ufc/l3info/oprog/TicketMachine.java
@@ -0,0 +1,25 @@
+package fr.ufc.l3info.oprog;
+
+public class TicketMachine {
+
+    private static final TicketMachine INSTANCE = new TicketMachine();
+
+    private TicketMachine() { }
+
+    public static TicketMachine getInstance() {
+        return INSTANCE;
+    }
+
+    ITicket buyTicket(boolean child, int... amount){
+        if(amount.length == 0) return new TenIllimitedTripsTicket(child);
+        if(amount[0] > 0) return new BaseTicket(child, amount[0]);
+        return  null;
+    }
+
+    ITicket adjustFare(ITicket old, int amount){
+        if(old == null) return null;
+        if (old instanceof TenIllimitedTripsTicket) return old;
+        if(amount <= 0) return old;
+        return new AdjustedTicket(old,amount);
+    }
+}
--- Transport/src/test/java/fr/ufc/l3info/oprog/TestTicketMachine.java
+++ Transport/src/test/java/fr/ufc/l3info/oprog/TestTicketMachine.java
@@ -8,16 +8,17 @@ public class TestTicketMachine {
     ITicket t;
     ITicket base;
     ITicket adjusted;
+    TicketMachine machine;
     @Before
     public void decl(){
-        base = buyTicket(false,100);
+        machine = TicketMachine.getInstance();
+        base = machine.buyTicket(false,100);
     }
 
     @Test
     public void buyTicketBaseTicket(){
-        t = buyTicket(true,10);
-        boolean bol = false;
-        if (t instanceof BaseTicket) bol = true;
+        t = machine.buyTicket(true,10);
+        boolean bol = t instanceof BaseTicket;
         Assert.assertTrue(bol);
         Assert.assertEquals(10,t.getAmount());
         Assert.assertTrue(t.isChild());
@@ -27,9 +28,8 @@ public class TestTicketMachine {
 
     @Test
     public void buyTicketTenIllimitedTripsTicket(){
-        t = buyTicket(true);
-        boolean bol = false;
-        if (t instanceof BaseTicket) bol = true;
+        t = machine.buyTicket(true);
+        boolean bol = t instanceof TenIllimitedTripsTicket;
         Assert.assertTrue(bol);
         Assert.assertEquals(2147483647,t.getAmount());
         Assert.assertTrue(t.isChild());
@@ -39,29 +39,49 @@ public class TestTicketMachine {
 
     @Test
     public void buyTicketNull(){
-        t = buyTicket(true,-600);
+        t = machine.buyTicket(true,-600);
         Assert.assertNull(t);
     }
 
     @Test
     public void adjustFareAjoutBaseTicket(){
-        adjusted = adjustFare(base,100);
-        boolean bol = false;
-        if (t instanceof AdjustedTicket) bol = true;
+        adjusted = machine.adjustFare(base,100);
+        boolean bol = adjusted instanceof AdjustedTicket;
         Assert.assertTrue(bol);
         Assert.assertEquals(200, adjusted.getAmount());
     }
 
     @Test
     public void adjustFareNull(){
-        adjusted = adjustFare(null,100);
+        adjusted = machine.adjustFare(null,100);
         Assert.assertNull(adjusted);
     }
 
     //TODO : Finir les tests adjustedFare somme ajout 0 ou inf et ticket non ajustable
     @Test
     public void adjustFareNoSuperieur0(){
+        adjusted = machine.adjustFare(base,0);
+        Assert.assertEquals(base.getClass(),adjusted.getClass());
+        Assert.assertEquals(base.isChild(), adjusted.isChild());
+        Assert.assertEquals(base.getEntryStation(),adjusted.getEntryStation());
+        Assert.assertEquals(base.getAmount(),adjusted.getAmount());
+        Assert.assertEquals(base.isValid(),adjusted.isValid());
+    }
 
+    public void adjustFareNoSNeg(){
+        adjusted = machine.adjustFare(base,-5);
+        Assert.assertEquals(base.getClass(),adjusted.getClass());
+        Assert.assertEquals(base.isChild(), adjusted.isChild());
+        Assert.assertEquals(base.getEntryStation(),adjusted.getEntryStation());
+        Assert.assertEquals(base.getAmount(),adjusted.getAmount());
+        Assert.assertEquals(base.isValid(),adjusted.isValid());
+    }
+
+    public void adjustFareNoSAdjust(){
+        t = new TenIllimitedTripsTicket(true);
+        adjusted = machine.adjustFare(t,-5);
+        boolean bol = t instanceof TenIllimitedTripsTicket;
+        Assert.assertTrue(bol);
     }
 
 
